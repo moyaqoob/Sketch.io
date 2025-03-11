4105cea775a05da3983ceff752ac826fdd57b858
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-03-12 00:58:20 +0530

    added favicons for frontend

--- apps/co-sketch-backend/src/index.ts
+++ apps/co-sketch-backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "welcome to CoSketch",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`[ server ] is listening on : http://localhost:${PORT}`);
+});
--- apps/co-sketch-frontend/src/app/page.tsx
+++ apps/co-sketch-frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center text-5xl font-extrabold w-screen h-screen">
+    <div className="flex justify-center items-center text-5xl font-extrabold w-screen h-screen text-primary">
       CoSketch frontend
     </div>
   );
