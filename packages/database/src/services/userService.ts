f8d636e0e5c8c63672ab7edf995c404c82a1edea
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-03-20 13:52:37 +0530

    added database

---
+++ packages/database/src/services/userService.ts
@@ -0,0 +1,9 @@
+import client from "..";
+
+export const createUser = async (
+  username: string,
+  hashedPassword: string,
+  name: string
+) => {};
+
+export const getUserByUser = async (username: string) => {};
