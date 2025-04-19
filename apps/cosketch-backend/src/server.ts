42b3dae84e4772bc315c382babfaefdf5cc13a15
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-04-20 05:09:21 +0530

    update: cors

--- apps/cosketch-backend/src/server.ts
+++ apps/cosketch-backend/src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", FRONTEND_URL],
+    origin: ["http://localhost:3000", "https://www.cosketch.xyz", FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
